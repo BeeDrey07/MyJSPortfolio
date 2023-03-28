f469759c00bfcbf87d316c6a38acbc6644b394e7
BeeDrey07 <126160535+BeeDrey07@users.noreply.github.com>
2023-03-28 21:41:34 +0200

    Add files via upload

---
+++ Chapter11/chapter11.js
@@ -0,0 +1,32 @@
+var people = ["Kareem", "Fatma", "Sumaya", "Mr. Hobson", "Ms. Young", "Mrs. O' Doherty"];
+var otherPeople = ["Emilie", "Mommy", "Dad", "Kathy"];
+
+// people = people.concat(otherPeople);
+
+// people = people.indexOf("Sumaya");
+
+// people = people.join(" # ");
+
+// people = people.lastIndexOf("Ms.Young");
+
+// people = people.pop();
+
+// people = people.push("Teddy");
+
+// people.push("Teddy");
+
+// people = people.reverse();
+
+// people.shift();
+
+// people.unshift("Teddy");
+
+// people = people.slice(0,3);
+
+// people = people.sort();
+
+// people.splice(1,0,"Cathy");
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
+
